331515b9de663b38774047e60d9b258d5e596c03
aricloverEXTRA <157071384+aricloverEXTRA@users.noreply.github.com>
2026-05-31 20:41:15 -0500

    Delete src directory

--- src/main/java/com/pvpranked/packet/WindChargeExplosionReader.java
+++
@@ -1,9 +0,0 @@
-package com.pvpranked.packet;
-
-import net.minecraft.network.listener.ClientPlayPacketListener;
-
-public abstract class WindChargeExplosionReader {
-    public static WindChargeExplosionReader reader;
-
-    public abstract void onExplosion(WindChargeExplosionS2CPacket packet, ClientPlayPacketListener listener);
-}
